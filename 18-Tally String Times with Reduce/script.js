e1e3e8fb8dd6005813c38f88d78c98830760aec3
Ross <rossgoodliffe@gmail.com>
2022-02-17 22:14:16 +0000

    lesson finished

---
+++ 18-Tally String Times with Reduce/script.js
@@ -0,0 +1,23 @@
+
+// Covert list of times into an array
+const timeNodes = [...document.querySelectorAll('[data-time]')];
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        // Seperating into mins and secs and coverting into float
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins * 60) + secs;  
+    })
+    .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+
+console.log(hours, mins, secondsLeft)
